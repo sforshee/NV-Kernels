4c05ddfcec5569d39078a1247aa2256a2853c929
Dave Jiang <dave.jiang@intel.com>
2026-06-29 15:10:58 -0700

    NVIDIA: VR: SAUCE: cxl/test: Add test for module parameters
    Add a test for module paraters during module init to make sure that
    only one is activated.
    
    [dj: Dropped counting fail_autoassemble modparm. (Alison) ]
    
    Suggested-by: Alison Schofield <alison.schofield@intel.com>
    Tested-by: Alison Schofield <alison.schofield@intel.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Link: https://patch.msgid.link/20260629221104.3891733-2-dave.jiang@intel.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    (backported from commit afae0fa7c6b163339a5d357942b89956a0820e45 linux-next)
    [ sforshee: adjust context for missing mock auto-region alignment
      check ]
    Signed-off-by: Seth Forshee <sforshee@nvidia.com>

--- tools/testing/cxl/test/cxl.c
+++ tools/testing/cxl/test/cxl.c
@@ -1829,10 +1829,28 @@ static struct attribute *cxl_acpi_attrs[] = {
 };
 ATTRIBUTE_GROUPS(cxl_acpi);
 
+static bool __init have_multiple_modparms(void)
+{
+	int count = 0;
+
+	if (interleave_arithmetic)
+		count++;
+	if (extended_linear_cache)
+		count++;
+	if (hmem_test)
+		count++;
+
+	return count > 1;
+}
+
 static __init int cxl_test_init(void)
 {
 	int rc, i;
 	struct range mappable;
+
+	/* Enforce a single module param active at a time */
+	if (have_multiple_modparms())
+		return -EINVAL;
 
 	cxl_acpi_test();
 	cxl_core_test();
